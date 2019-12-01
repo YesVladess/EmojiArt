c5a1b45d5360d7977591c507aad0dc72a41d2427
YesVladess <vlad.kononenko95@gmail.com>
2019-12-02 00:51:22 +0300

    New icon + UIDocumentBrowser stuff

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -2,57 +2,20 @@
 //  EmojiArtView.swift
 //  EmojiArt
 //
-//  Created by YesVladess on 22.11.2019.
-//  Copyright © 2019 YesVladess. All rights reserved.
+//  Created by Ruben on 1/12/18.
+//  Copyright © 2018 Ruben. All rights reserved.
 //
 
 import UIKit
 
+///
 /// View for EmojiArt. Contains/shows the given image.
-class EmojiArtView: UIView, UIDropInteractionDelegate {
+///
+class EmojiArtView: UIView {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
-    }
-    
-    private func setup() {
-        addInteraction(UIDropInteraction(delegate: self))
-    }
-    
-    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
-        return session.canLoadObjects(ofClass: NSAttributedString.self)
-    }
-    
-    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
-        return UIDropProposal(operation: .copy)
-    }
-    
-    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
-        session.loadObjects(ofClass: NSAttributedString.self) { providers in
-            let dropPoint = session.location(in: self)
-            for attributedString in providers as? [NSAttributedString] ?? [] {
-                self.addLabel(with: attributedString, centeredAt: dropPoint)
-            }
-        }
-    }
-    
-    private func addLabel(with attributedString: NSAttributedString, centeredAt point: CGPoint) {
-        let label = UILabel()
-        label.backgroundColor = .clear
-        label.attributedText = attributedString
-        label.sizeToFit()
-        label.center = point
-        addEmojiArtGestureRecognizers(to: label)
-        addSubview(label)
-    }
-    
+    ///
     /// The background image
+    ///
     var backgroundImage: UIImage? {
         didSet {
             // When image is set, we need to re-draw ourselves
@@ -60,8 +23,89 @@ class EmojiArtView: UIView, UIDropInteractionDelegate {
         }
     }
 
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Set backgroundImage as the background
         backgroundImage?.draw(in: bounds)
+    }
+    
+    // Init with frame
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    // Init with coder
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    ///
+    /// Do initialization setup. Called after init(frame:) and init(coder:)
+    ///
+    private func setup() {
+        // Register `self` to accept drop interactions
+        addInteraction(UIDropInteraction(delegate: self))
+    }
+    
+    ///
+    /// Add UILabel with the given `attributedString` and centered at `centerPoint`
+    ///
+    func addLabel(with attributedString: NSAttributedString, centeredAt centerPoint: CGPoint) {
+        let label = UILabel()
+        
+        // Setup label's color, text, size, etc.
+        label.backgroundColor = .clear
+        label.attributedText = attributedString
+        label.sizeToFit()
+        label.center = centerPoint
+        
+        // Add gesture recognizers to the label (i.e. pan to move, pinch to zoom, etc.)
+        addEmojiArtGestureRecognizers(to: label)
+        
+        // Add subview
+        self.addSubview(label)
+    }
+}
+
+//
+// Make `EmojiArtView` conform to `UIDropInteractionDelegate`
+//
+extension EmojiArtView: UIDropInteractionDelegate {
+    
+    //
+    // What to do when performing a drop
+    //
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        // Copy item(s)
+        return UIDropProposal(operation: .copy)
+    }
+    
+    //
+    // Determine if we can accept the given drop/session
+    //
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    //
+    // Perform the given drop
+    //
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        
+        // Process any dropped attributed strings
+        session.loadObjects(ofClass: NSAttributedString.self) { providers in
+            
+            // The location where the drop happens. Used to drop label in correct place
+            let dropPoint = session.location(in: self)
+            
+            // Process each dropped attributed string
+            for attributedString in providers as? [NSAttributedString] ?? [] {
+                // Add a label with the dropped string
+                self.addLabel(with: attributedString, centeredAt: dropPoint)
+            }
+        }
     }
 }
